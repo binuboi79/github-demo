736adb33ec2021d9236c78bf6363e1b2097d27ab
anurag_baral <anurag_baral@persistent.com>
2021-06-16 15:02:06 +0530

    Added function headers

--- math.py
+++ math.py
@@ -1,13 +1,17 @@
 #Add addition implementation
+#new head
 def add(x,y):
     return x+y
 #Add subtraction implementation
+#new head
 def subtract(x,y):
     return x-y
 #Add multiplication implementation
+#new head
 def multiply(x,y):
     return x*y
 #Add division implementation
+#new head
 def divide(x,y):
     if x<0:
 	return NEGATIVE_ANS
